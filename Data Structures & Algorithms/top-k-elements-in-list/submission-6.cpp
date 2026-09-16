08dfc17b6edfbf8c07e823625d7ee959dd6351ac
GAGAN AGGARWAL <gaganag50@gmail.com>
2026-09-16 19:06:32 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(auto i: nums){
+            mp[i]++;
+        }
+        vector<pair<int,int>> v;
+        for(auto it: mp){
+            v.emplace_back(it.second, it.first);
+        }
+        sort(v.rbegin(), v.rend());
+        vector<int> ans;
+        for(int i = 0; i < k; ++i) {
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
